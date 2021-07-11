a24f4e3c9fa95ea1847e0c8b4f07682a9bf0b832
Deepak <dwb508@gmail.com>
2021-07-11 09:41:16 +0530

    m

--- src/main/java/com/app/SpringBootMain.java
+++ src/main/java/com/app/SpringBootMain.java
@@ -2,11 +2,17 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SpringBootMain {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMain.class, args);
+	}
+	
+	public String getResponse(){
+		return "Hello World"; 
+		
 	}
 }
